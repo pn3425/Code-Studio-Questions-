3493df0c94fa8d0b530c1b2583165dd6389d23b1
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2023-08-19 16:22:33 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,5 +1,6 @@
 int removeDuplicates(vector<int> &arr, int n) {
 	// Write your code here.
+//Approach 1(TW0 POINTER)
    int i=0;
    for(int j=1;j<n;j++)
    {
@@ -11,5 +12,20 @@ int removeDuplicates(vector<int> &arr, int n) {
 
    }
   return i+1;
+
+//Approach 2(USING SETS)
+	 // set<int>st;
+  // for(int i=0;i<n;i++)
+  // {
+  //   st.insert(arr[i]);
+  // }
+
+  // int k=st.size();
+
+  // int j = 0;
+  // for (int x: st) {
+  //   arr[j++] = x;
+  // }
+  // return k;
 
 }
