1e47de694c9c0355ddff6bcad2d768f7d15ae86e
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-04-08 22:15:33 +0530

    Create Increasing Number Triangle.cpp
    Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Number Triangle.
    
    Example:
    Input: ‘N’ = 3
    
    Output: 
    
    1
    2 3
    4 5 6

---
+++ Pattern/Increasing Number Triangle.cpp
@@ -0,0 +1,14 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    int num=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+}
