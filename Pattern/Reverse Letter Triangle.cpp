8604af6ef5a795cc5fcc076350e3b9d65fb6d7a8
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-04-08 22:46:03 +0530

    Create  Reverse Letter Triangle.cpp
    Problem statement
    Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Reverse Letter Triangle.
    
    You must print a matrix corresponding to the given Reverse Letter Triangle.
    
    Example:
    Input: ‘N’ = 3
    
    Output: 
    
    A B C
    A B
    A

---
+++ Pattern/Reverse Letter Triangle.cpp
@@ -0,0 +1,13 @@
+void nLetterTriangle(int n) {
+    // Write your code here.
+    for(int i=1;i<=n;i++)
+    {
+        char ch = 'A';
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+}
