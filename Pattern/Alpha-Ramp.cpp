465e7ba6302457244fbbdf608f44a4d3edffefc9
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-04-08 22:55:14 +0530

    Create Alpha-Ramp.cpp
    Sam is curious about Alpha-Ramp, so he decided to create Alpha-Ramp of different sizes.
    
    An Alpha-Ramp is represented by a triangle, where alphabets are filled from the top in order.
    
    For every value of ‘N’, help sam to return the corresponding Alpha-Ramp.
    
    Example:
    Input: ‘N’ = 3
    
    Output: 
    A
    B B
    C C C
    
    https://www.naukri.com/code360/problems/alpha-ramp_6581888?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=PROBLEM

---
+++ Pattern/Alpha-Ramp.cpp
@@ -0,0 +1,13 @@
+void alphaRamp(int n) {
+    // Write your code here.
+    char ch = 'A';
+    for(int i=0;i<n;i++)
+    {
+       for(int j=0;j<=i;j++)    
+        {
+            cout<<ch<<" ";  
+        }
+        ch++;
+        cout<<endl;
+    }
+}
