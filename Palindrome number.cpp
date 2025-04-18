840b49ebf0ac10878fddedfeac7b4ff7152036aa
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-04-18 22:17:43 +0530

    Create Palindrome number.cpp
    Check whether a given number ’n’ is a palindrome number.
    
    Note :
    Palindrome numbers are the numbers that don't change when reversed.
    You don’t need to print anything. Just implement the given function.
    Example:
    Input: 'n' = 51415
    Output: true
    Explanation: On reversing, 51415 gives 51415.
    
    https://www.naukri.com/code360/problems/palindrome-number_624662?leftPanelTabValue=PROBLEM

---
+++ Palindrome number.cpp
@@ -0,0 +1,26 @@
+bool palindrome(int n)
+{
+    if( n == 0 )
+    {
+        return true;
+    }
+    // Write your code here
+    int dup=n;
+    int revNum=0;
+    int r;
+    while(n>0)
+    {
+      r = n % 10;
+      revNum = (10*revNum)+r;
+      n = n / 10;
+    }
+
+    if(dup == revNum)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
