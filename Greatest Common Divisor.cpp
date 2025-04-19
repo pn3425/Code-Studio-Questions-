a8103f8f9df83501ff5a45bd1ef76beb23b19f55
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-04-19 18:51:58 +0530

    Create  Greatest Common Divisor.cpp
    Sample Input 1:
    2
    20 15
    8 32
    Sample Output 1:
    5
    8
    
    Multiple Approach : 
    1) My Approach - Not Passed Completely
    2) Striver - Partially Passed 
    3) Striver - Using Eculidean

---
+++ Greatest Common Divisor.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h> 
+int findGcd(int x, int y)
+{
+    // Write your code here
+    //Final Approach using Euclidean
+    while(x>0 && y>0)
+    {
+      if(x>y) x = x % y;
+      else
+      y = y % x;
+    }
+
+    if(x==0)
+    return y;
+    else
+    return x;
+}
+
+
+
+/*
+My Approach  : Approach 1 (Not Passing)
+
+   vector<int> x1;
+    vector<int> y1;
+    for(int i=1;i<=sqrt(x);i++)
+    {
+        if ( x % i == 0)
+          x1.push_back(i);
+          if( (x/i) != i )
+            x1.push_back(x/i);
+    }
+   
+    for(int i=1;i<=sqrt(y);i++)
+    {
+         if ( y % i == 0)
+          y1.push_back(i);
+          if( (y/i) != i )
+            y1.push_back(y/i);  
+    }
+
+    sort(x1.begin(), x1.end(),greater<int>());
+    sort(y1.begin(), y1.end(),greater<int>());
+
+    for(auto i : x1)
+    {
+       if (find(y1.begin(), y1.end(), i) != y1.end())
+       {
+            return i;
+       }
+    }
+    return 1;
+
+
+Striver Approach 1 : (Partially Accepted, Brute Force, Linear TC)
+
+  for(int i=min(x,y); i>=1; i--)
+  {
+    if(x%i==0 && y%i==0)
+    {
+      return i;
+      break;
+    }
+  }
+  return 1;
+
+
+Striver Approach 2 : Euclidean 
+
+
+
+*/
