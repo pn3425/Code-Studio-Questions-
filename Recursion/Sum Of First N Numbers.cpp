f2ee3f8714999e6303b78f58dfdc22d457353036
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-04-29 21:20:53 +0530

    Update Sum Of First N Numbers.cpp

--- Recursion/Sum Of First N Numbers.cpp
+++ Recursion/Sum Of First N Numbers.cpp
@@ -1,5 +1,12 @@
+//Recursive Approach
 long long sumFirstN(long long n) {
     // Write your code here.
     if(n == 0 ) return 0;
     return n+sumFirstN(n-1); 
 }
+
+//Normal Approach
+
+   long long ans;
+    ans = n * (n+1)/2;
+    return ans;
