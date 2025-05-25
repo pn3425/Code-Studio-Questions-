3f25dae9e77f08f344aa26d20d16a8b962fb39f7
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-05-25 23:42:52 +0530

    Create Rotate Array.cpp
    Given an array 'arr' with 'n' elements, the task is to rotate the array to the left by 'k' steps, where 'k' is non-negative.
    
    Example:
    'arr '= [1,2,3,4,5]
    'k' = 1  rotated array = [2,3,4,5,1]
    'k' = 2  rotated array = [3,4,5,1,2]
    'k' = 3  rotated array = [4,5,1,2,3] and so on.
    
    https://www.naukri.com/code360/problems/rotate-array_1230543

---
+++ Array/Rotate Array.cpp
@@ -0,0 +1,30 @@
+//STRIVER APPROACHES
+//App1 - Brute Force 
+
+/*
+ vector<int> rotateArray(vector<int>arr, int k) {
+   int n = arr.size();
+   k = k%n; 
+  vector<int> temp;
+
+  //Put the first k elements from main array to temp array
+  //Example Array -> {1,2,3,4,5}
+  //Here k=k%n -> k=3
+   for(int i=0;i<k;i++)
+   {
+       temp.push_back(arr[i]);
+   }
+
+   //Shifting
+   for(int i=k;i<n;i++)
+   {
+       arr[i-k]=arr[i]; 
+   }
+
+   //Now using temp, put those remaining element in correct position in arr[]
+   for(int i=n-k;i<n;i++)
+   {
+       arr[i]=temp[i-(n-k)];
+   }
+  return arr;
+}  */
