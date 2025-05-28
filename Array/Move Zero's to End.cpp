d30bb0cf8f970ee231ddf0133036fe0b60b6d4f1
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-05-28 23:05:11 +0530

    Update Move Zero's to End.cpp

--- Array/Move Zero's to End.cpp
+++ Array/Move Zero's to End.cpp
@@ -52,3 +52,33 @@ vector<int> moveZeros(int n, vector<int> a) {
 
  return a;
  }
+
+// Approach 2 (Using 2 Pointer)
+  vector<int> moveZeros(int n, vector<int> a){
+       
+       int j=-1;                       //This is to set j=0 always and then compare with the right elements and check if it is non zero, if it is then swap
+       for(int i=0;i<n;i++)
+       {
+           if(a[i]==0)
+           {
+               j=i;
+               break;
+           }
+   
+       }
+       if(j==-1)return a; //For Non-Zero, since j will be 0 if any zeroes present
+
+       for(int i=j+1;i<n;i++)
+       {
+           if(a[i]!=0)
+           {
+               swap(a[i],a[j]);
+               j++;
+           }
+        
+       
+       }
+
+    return a;
+       
+  }
