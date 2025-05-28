dde56cdb9e24118e835f1d991ac684e6290a23f6
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-05-28 22:16:05 +0530

    Create Move Zero's to End.cpp
    Given an array 'arr' of 'n' non-negative integers, your task is to move all the zeros to the end of the array while keeping the non-zero elements at the start of the array in their original order. Return the modified array.
    
    Example :
    Input: ‘n’ = 5, ‘arr’ = [1, 2, 0, 0, 2, 3]
    Output: [1, 2, 2, 3, 0, 0]
    
    Explanation: Moved all the 0’s to the end of an array, and the rest of the elements retain the order at the start.
    
    https://www.naukri.com/code360/problems/ninja-and-the-zero-s_6581958?leftPanelTabValue=PROBLEM

---
+++ Array/Move Zero's to End.cpp
@@ -0,0 +1,54 @@
+/*
+vector<int> moveZeros(int n, vector<int> a) {
+    // Write your code here.
+    vector<int>st;
+    int zerocnt=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i] == 0)
+        {
+            zerocnt++;
+        }
+    }
+    int l = a.size();
+    
+    for(int i=0;i<n;i++)
+    {
+        if (a[i] != 0) {
+            st.push_back(a[i]);
+        }
+    }
+
+      for (int i = 0; i < zerocnt; i++) {
+        st.push_back(0);
+    }
+    return st;
+}*/
+
+//------------------------------STRIVER----------------------------------------
+
+//APP 1 BRUTE FORCE TC & SC -> o(n) WORST CASE 
+
+ vector<int> moveZeros(int n, vector<int> a) {
+
+ vector<int> temp;
+ for(int i=0;i<n;i++)
+ {
+     if(a[i]!=0)
+     {
+         temp.push_back(a[i]);
+     }
+ }
+
+ for(int i=0;i<temp.size();i++)
+ {
+    a[i]=temp[i];
+ }
+
+ for(int i=temp.size();i<n;i++)
+ {
+     a[i]=0;
+ }
+
+ return a;
+ }
