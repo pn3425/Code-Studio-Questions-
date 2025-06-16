885a37889dc660fe68030c51c9659dce3ff64e09
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-06-17 00:20:53 +0530

    Create Longest Subarray With Sum K.cpp
    Problem statement
    You are given an array 'a' of size 'n' and an integer 'k'.
    
    Find the length of the longest subarray of 'a' whose sum is equal to 'k'.
    
    Example :
    Input: ‘n’ = 7 ‘k’ = 3
    ‘a’ = [1, 2, 3, 1, 1, 1, 1]
    
    Output: 3
    
    Explanation: Subarrays whose sum = ‘3’ are:
    
    [1, 2], [3], [1, 1, 1] and [1, 1, 1]
    
    Here, the length of the longest subarray is 3, which is our final answer.
    
    https://www.naukri.com/code360/problems/longest-subarray-with-sum-k_6682399?leftPanelTabValue=PROBLEM

---
+++ Array/Longest Subarray With Sum K.cpp
@@ -0,0 +1,82 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    
+  //Approach 1 : Brute Force (TC -> O(N^3), SC -> O(n))
+  // Create sub arrays and check for the sum
+
+  /*
+  int len=0;
+  for(int i=0;i<a.size();i++)
+  {
+    for(int j=i;j<a.size();j++)
+    {
+      int sum=0;
+      for(int m=i;m<=j;m++)   //Iterate from i->j and then get the sum
+      {
+        sum = sum + a[m];
+      }
+       if(sum == k) len=max(len,j-i+1);  
+    }
+  }
+  return len;*/
+
+  //Approach 1.1 -> O(N^2)
+
+    /*
+  int len=0;
+  for(int i=0;i<a.size();i++)
+  {
+    int sum=0;
+    for(int j=i;j<a.size();j++)
+    {
+     sum = sum + a[j];
+     if(sum == k) len=max(len,j-i+1);  
+    }
+  }
+  return len;*/
+
+  //Approach 2 : Prefix Sum (If sum is k, then we check for x-k) (Did not understand)
+  // This approach is correct for positives and negatives 
+
+/*
+  map<long long, int> preSumMap;
+  long long sum = 0;
+  int maxlen = 0;
+  for(int i=0;i<a.size();i++){
+    sum=sum+arr[i];
+    if(sum==k)
+    {
+      maxlen = max(maxlen,i+1);
+    }
+  long long rem = sum - k;
+  if(preSumMap.find(rem) != preSumMap.end())
+  
+   */
+
+  //Approach 3 : Optimal (2 Pointer)
+  // Move forward(R), check if sum>k, if YES then subtract the current sum with arr[left] and left++
+  // So you move forward and check sum and trim the left part
+  //This code is only valid for POSITIVES
+  //TC -> O(2N) SC-> O(1)
+
+  int left = 0, right = 0;
+  long long sum = a[0];
+  int maxlen = 0;
+  int n = a.size();
+
+  while(right<n){
+
+    while(left <= right && sum > k)
+    {
+      sum = sum - a[left];
+      left++;
+    }
+    
+    if(sum==k){
+      maxlen = max(maxlen, right - left + 1);
+    }
+
+    right++;
+    if(right<n)sum = sum + a[right];
+  }
+    return maxlen;
+  }
