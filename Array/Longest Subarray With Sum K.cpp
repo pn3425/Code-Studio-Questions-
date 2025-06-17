f52fdd5e3fb2fe5c3e048f6ce4c6439d71485da5
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-06-17 22:37:29 +0530

    Update Longest Subarray With Sum K.cpp

--- Array/Longest Subarray With Sum K.cpp
+++ Array/Longest Subarray With Sum K.cpp
@@ -38,17 +38,27 @@ int longestSubarrayWithSumK(vector<int> a, long long k) {
   // This approach is correct for positives and negatives 
 
 /*
-  map<long long, int> preSumMap;
-  long long sum = 0;
-  int maxlen = 0;
-  for(int i=0;i<a.size();i++){
-    sum=sum+arr[i];
-    if(sum==k)
-    {
-      maxlen = max(maxlen,i+1);
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxlen = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        sum = sum + arr[i];
+
+        if(sum == k) {
+            maxlen = max(maxlen, i + 1);
+        }
+
+        long long rem = sum - k;
+        if(preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxlen = max(maxlen, len);
+        }
+
+        if(preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
     }
-  long long rem = sum - k;
-  if(preSumMap.find(rem) != preSumMap.end())
+    return maxlen;
   
    */
 
