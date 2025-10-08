4fadedc25817b1982cd33197fdf535f848f2ae7e
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-10-09 00:53:19 +0530

    Create Merge All Overlapping Intervals.cpp
    For example:
    For ‘N’ = 4, and 
    ‘Arr’ = {{1, 3}, {2, 4}, {3, 5}, {6, 7}}
    We can see that {1, 3} and {2, 4} overlap, so if we merge them, we get {1, 4}.
    Now ‘Arr’ becomes: {{1, 4}, {3, 5}, {6, 7}}
    Still, we observe that {1, 4} and {3, 5} overlap, hence we merge them into {1, 5}.
    Hence, ‘Arr’ becomes {{1, 5}, {6, 7}}.
    
    https://www.naukri.com/code360/problems/merge-all-overlapping-intervals_6783452?leftPanelTabValue=PROBLEM

---
+++ Array/Hard Problems/Merge All Overlapping Intervals.cpp
@@ -0,0 +1,52 @@
+#include<vector>
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
+	// Write your code here.
+
+	// Brute Force : TC ->Nlogn+ O(2n)
+	int n = arr.size();
+	sort(arr.begin(),arr.end());
+	vector<vector<int>> ans;
+	for(int i=0;i<n;i++)
+	{
+		int start = arr[i][0];
+		int end = arr[i][1];
+		if( !ans.empty() && end <= ans.back()[1]){
+			continue;
+		}
+		for(int j=i+1;j<n;j++){
+			if(arr[j][0] <= end){
+				end = max(end, arr[j][1]);
+			}
+			else
+			{
+				break;
+			}
+		}
+		ans.push_back({start,end});
+	}
+	return ans;
+
+  // Optimised TC - NLOGN, SC - O(N)
+	/*
+	int n = arr.size();
+	sort(arr.begin(),arr.end());
+	vector<vector<int>> ans;
+	for(int i=0;i<n;i++)
+	{
+		if(ans.empty() || arr[i][0] > ans.back()[1]){
+			ans.push_back(arr[i]);
+		}
+		else
+		{
+			ans.back()[1]=max(ans.back()[1], arr[i][1]);
+		}
+
+	}
+	return ans;
+
+
+
+
+	*/
+}
