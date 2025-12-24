730994d99d620be7ddb0479965fc249d6613271b
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-12-24 17:49:36 +0530

    Create  Kth Missing Positive Number.cpp
    You are given a strictly increasing array 'vec' and a positive integer 'k'.
    
    
    
    Find the 'kth' positive integer missing from 'vec'.
    
    
    
    Example :
    Input: vec = [2,4,5,7] , k = 3
    
    Output: 6
    
    Explanation : 
    In the given example, first missing positive integer is 1 second missing positive integer is 3, and the third missing positive integer is 6.
    Hence the answer is 6. 
    
    https://www.naukri.com/code360/problems/kth-missing-element_893215?leftPanelTabValue=PROBLEM

---
+++ Binary Search/BS on Answers/Kth Missing Positive Number.cpp
@@ -0,0 +1,17 @@
+int missingK(vector < int > vec, int n, int k) {
+    // Write your code here.
+    // Approach : Brute Force -> TC : O(log2 N)
+    int low = 0, high = n - 1;
+     
+    while(low <= high){
+        int mid = (low + high)/2;
+        int missing = vec[mid] - (mid + 1);
+        if(missing < k){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return k + high + 1;
+}
